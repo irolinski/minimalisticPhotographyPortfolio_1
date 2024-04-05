27df563e23ce68d158d4ab20f436c721f5628736
szczek <ignacy.rolinski@gmail.com>
2024-04-05 18:23:36 +0200

    feat(projectPage): add basic swipe functionality;

--- src/Components/Projects/Carousel.tsx
+++ src/Components/Projects/Carousel.tsx
@@ -20,11 +20,31 @@ export default function Carousel ({slides}: carouselTypes) {
       if (current === slides.length - 1) setCurrent(0);
       else setCurrent(current + 1);
     };
+    
+    const [touchStart, setTouchStart] = useState(null)
+    const [touchEnd, setTouchEnd] = useState(null)
 
+    const minSwipeDistance = 100 
+
+    const onTouchStart = (evt: any) => {
+      setTouchEnd(null) // otherwise the swipe is fired even with usual touch events
+      setTouchStart(evt.targetTouches[0].clientX);
+    }
+
+    const onTouchMove = (evt: any) => setTouchEnd(evt.targetTouches[0].clientX);
+
+    const onTouchEnd = () => {
+      if (!touchStart || !touchEnd) return
+      const distance = touchStart - touchEnd
+      const isLeftSwipe = distance > minSwipeDistance
+      const isRightSwipe = distance < -minSwipeDistance
+      isLeftSwipe && nextSlide();
+      isRightSwipe && previousSlide();
+    }
 
     return(
       <>
-        <div className="flex mb-[25%] sm:mb-[10%] lg:my-[5%]" >
+        <div className="flex mb-[25%] sm:mb-[10%] lg:my-[5%]" onTouchStart={onTouchStart} onTouchMove={onTouchMove} onTouchEnd={onTouchEnd} >
             <div className='carousel-wrapper px-2 mx-auto flex'>      
                 <button onClick={previousSlide}> &lt; </button>
                 <div className="overflow-hidden relative max-w-full md:max-w-screen-md mx-8">
