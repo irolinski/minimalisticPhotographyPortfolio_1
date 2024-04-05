ce5317df08b4fc51120a12a71adea20b7120c861
szczek <ignacy.rolinski@gmail.com>
2024-04-05 17:33:42 +0200

    styles(aboutPage): style, add responisveness

--- src/Pages/Projects/AboutPage.tsx
+++ src/Pages/Projects/AboutPage.tsx
@@ -4,14 +4,14 @@ export default function AboutPage () {
 
     return(
         <div className="flex flex-wrap mx-auto justify-around sm:p-16 mx-4"> 
-            <img className='max-w-96 max-h-96 px-24 py-8 lg:px-16' src='https://upload.wikimedia.org/wikipedia/commons/thumb/0/0d/La_velada%2C_por_Rafael.jpg/1200px-La_velada%2C_por_Rafael.jpg' />
-            <div className="font-header py-10 px-16 lg:p-10 my-1 lg:my-4">
-                <p className="pt-16 text-md border-t-2 md:px-16 lg:py-8 lg:border-t-0 lg:border-l-2 border-color-gray">
+            <img className='max-w-72 max-h-72  py-8 px-16 lg:px-16 mx-auto self-center' src='https://upload.wikimedia.org/wikipedia/commons/thumb/0/0d/La_velada%2C_por_Rafael.jpg/1200px-La_velada%2C_por_Rafael.jpg' />
+            <div className="about-text font-header py-10 px-12 lg:p-10 my-1 lg:my-4">
+                <p className="pt-16 text-md md:px-16 lg:py-8 lg:px-24">
                     Nazywam się Pola.<br /> Jestem fotografką/reporterką. <br /><br />
                     Otrzymałam tytuł inżyniera na Uniwersytecie Gdańskim.  <br /><br />
                     Obecnie przyjmuję zlecenia. <br /> <br /> 
                     Kontakt: <br />
-                    pola.brogosiewicz@gmail.com  <br /> <br />
+                    <a href="mailto:pola.brogosiewicz@gmail.com">pola.brogosiewicz@gmail.com</a>  
 
                 </p>
             </div>
