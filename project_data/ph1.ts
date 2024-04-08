c8c0b8e6a778564b3398b2fa6f3d37b2b677c84e
szczek <ignacy.rolinski@gmail.com>
2024-04-08 19:28:22 +0200

    Updates

---
+++ project_data/ph1.ts
@@ -0,0 +1,27 @@
+
+export const name = "Photos I";
+
+export const slides = [
+    "https://res.cloudinary.com/dtjtqp7r1/image/upload/v1712437326/z-Ola%20K.-Portfolio/PHOTO%201/y5xvijbcdbhn3pwmjyrg.jpg",
+    "https://res.cloudinary.com/dtjtqp7r1/image/upload/v1712437334/z-Ola%20K.-Portfolio/PHOTO%201/onmqegkuz24s6jlwfmga.jpg",
+    "https://res.cloudinary.com/dtjtqp7r1/image/upload/v1712437324/z-Ola%20K.-Portfolio/PHOTO%201/tuko0cf51nyi1bz5dpwr.jpg",
+    "https://res.cloudinary.com/dtjtqp7r1/image/upload/v1712437331/z-Ola%20K.-Portfolio/PHOTO%201/nvk1d6qujhfaqdvagmbp.jpg",
+    "https://res.cloudinary.com/dtjtqp7r1/image/upload/v1712437328/z-Ola%20K.-Portfolio/PHOTO%201/fimjhngvhmwfhwjpu8ii.jpg",
+    "https://res.cloudinary.com/dtjtqp7r1/image/upload/v1712437335/z-Ola%20K.-Portfolio/PHOTO%201/ldpsfmcecgc2vshncmyb.jpg",
+    "https://res.cloudinary.com/dtjtqp7r1/image/upload/v1712437331/z-Ola%20K.-Portfolio/PHOTO%201/k5wlh7kxckifmef4ijzf.jpg",
+    "https://res.cloudinary.com/dtjtqp7r1/image/upload/v1712437330/z-Ola%20K.-Portfolio/PHOTO%201/a6ujd28xxrdif0kmz3mj.jpg",
+    "https://res.cloudinary.com/dtjtqp7r1/image/upload/v1712437335/z-Ola%20K.-Portfolio/PHOTO%201/uk6dgtsbhro11pvwmou5.jpg",
+]; 
+
+const url = '/#/projekty/' + `${name.replace(' ', '_')}`;
+
+const imgUrl = "https://res.cloudinary.com/dtjtqp7r1/image/upload/v1712437334/z-Ola%20K.-Portfolio/PHOTO%201/onmqegkuz24s6jlwfmga.jpg";
+
+export const ph1 = {
+    name: name,
+    slides: slides,
+    url: url,
+    imgUrl: imgUrl
+}
+
+
