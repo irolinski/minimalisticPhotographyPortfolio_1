f61b47962f562073a5ffd61544e383e6bad39d89
szczek <ignacy.rolinski@gmail.com>
2024-04-24 00:03:59 +0200

    styles(about_me): change picture and fix its positioning;

--- src/Pages/Projects/AboutPage.tsx
+++ src/Pages/Projects/AboutPage.tsx
@@ -2,11 +2,11 @@ export default function AboutPage() {
   return (
     <div className="flex flex-wrap mx-auto justify-around sm:p-16 mx-4 mb-36 mt-24 lg:my-0 lg:py-[8vh] lg:flex-nowrap">
       <img
-        className="max-w-96 max-h-96 py-8 px-16 lg:px-16 mx-auto self-center xs:-translate-y-12 xl:-translate-y-0"
-        src="https://res.cloudinary.com/dtjtqp7r1/image/upload/v1712534970/z-Ola%20K.-Portfolio/i7xao8etuufkbwb0t3cv.jpg"
+        className="max-w-96 max-h-96 py-8 px-16 lg:px-16 mx-auto self-center xs:-translate-y-12 lg:-translate-y-0 hover:cursor-none"
+        src="https://res.cloudinary.com/dtjtqp7r1/image/upload/v1713909540/z-Ola%20K.-Portfolio/epbq3ohpz26q4aq5uyut.jpg"
       />
       <div className="about-text font-header py-10 px-8 sm:px-12 lg:p-10 my-1 lg:my-4">
-        {/* i deleted ml-36 for it looks weird */}
+        {/* i deleted ml-36 for it looked weird */}
         <p className="pt-16 text-md md:px-16 lg:py-8 lg:px-24">
           <span className="text-justify">
             Mam na imię Ola.&nbsp; Jestem fotografką i operatorką. <br />
